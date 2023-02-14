28d93ee6d6061c076a7470cc0addbc2cfd7a7ab7
SATYIK PRITAM YOGI <satyik_ug@cse.nits.ac.in>
2023-02-14 21:11:48 +0000

    Update 5 files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Typography } from '@mui/material';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <h1>Langing</h1>
-              </>
-            }
-          ></Route>
-          <Route path="/login" element={<h1>Login</h1>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <div>
+    <div>
+      <>
+    <div>
+  </div>;
 }
 
 export default App;
